d298656c3f2565f55639a4b205ee19b2ab7b499e
TPIOS <116010062@link.cuhk.edu.cn>
2019-04-17 13:18:53 +0800

    Update help.py
    现在在群内使用.help会私发给请求该命令的用户了

--- awesome/plugins/help.py
+++ awesome/plugins/help.py
@@ -2,7 +2,7 @@ from nonebot import on_command, CommandSession
 @on_command("help", only_to_me = False)
 async def help(session: CommandSession):
     readme = await get_readme()
-    await session.send(readme)
+    await session.send(readme, ensure_private = True)
 
 async def get_readme():
     res = '''
@@ -10,7 +10,7 @@ async def get_readme():
     这是现在版本所能提供的命令：
     .jrrp: 获得今天的人品值，每个人一天的人品值一定是固定的
     .ra[空格][某事件][概率值]: 对某一事件进行玄学概率分析，从而得到结果，目前已经支持概率值之前有数字，概率值应不大于100，每个人在一天的不同时间测试相同的东西，概率也可能是不同的
-    .help: 返回QQ-Bot的说明
+    .help: 现在，在群里使用这个命令，本Bot会私发给请求该命令的用户，QQ-Bot的说明
     --------
     接下来本Bot还会更新的内容：
     1. FGOwiki API连接
