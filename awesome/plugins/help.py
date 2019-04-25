606c5b43a072fbe0b4fedc64290411714bb50eec
TPIOS <116010062@link.cuhk.edu.cn>
2019-04-25 13:28:27 +0800

    update help.py

--- awesome/plugins/help.py
+++ awesome/plugins/help.py
@@ -5,13 +5,15 @@ async def help(session: CommandSession):
     await session.send(readme, ensure_private = True)
 
 async def get_readme():
-    res = '''这里是由望村小居士(TPIOS)编写的QQ-Bot
+    res = '''这里是由望村小居士(TPIOS)编写的QQ-Bot，
+    你可以亲切地叫我居娘
     这是现在版本所能提供的命令：
     .jrrp: 获得今天的人品值，每个人一天的人品值一定是固定的
     .ra[空格][某事件][概率值]: 对某一事件进行玄学概率分析，从而得到结果，目前已经支持概率值之前有数字，概率值应不大于100，每个人在一天的不同时间测试相同的东西，概率也可能是不同的
     .help: 现在，在群里使用这个命令，本Bot会私发给请求该命令的用户，QQ-Bot的说明
+    本居娘还搭载了图灵机器人功能，私聊居娘获得惊喜哦~
     --------
-    接下来本Bot还会更新的内容：
+    接下来居娘还会更新的内容：
     1. FGOwiki API连接
     2. 着手准备.dnd功能的设计'''
     return res
--- awesome/plugins/welcome.py
+++ awesome/plugins/welcome.py
@@ -1,4 +1,4 @@
 from nonebot import on_notice, NoticeSession
 @on_notice('group_increase')
 async def _(session: NoticeSession):
-    await session.send("欢迎进群，请自习阅读群公告", at_sender = True)
+    await session.send("欢迎进群，请仔细阅读群公告哦~", at_sender = True)
